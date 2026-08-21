914b69bcb5026bd115eda22edf40f994a9a3535d
Behruzbek <102900513+IsroilovBehruzbek@users.noreply.github.com>
2026-08-21 07:00:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?
-echo "bot ishlayapti";
-?>
+<?php
+
+$token = getenv("BOT_TOKEN");
+
+if (!$token) {
+    die("BOT_TOKEN TOPILMADI");
+}
+
+$url = "https://api.telegram.org/bot" . $token . "/getMe";
+
+$result = file_get_contents($url);
+
+header("Content-Type: application/json");
+echo $result;
